b1f4d14e370db55283088c675c291da0d724781a
Antonio Molinari <molinari@incode.it>
2018-04-17 18:16:46 +0200

    new page templates, loop templates css and scss scaffold

---
+++ wp-content/themes/trovacincitta/loop-templates/content-help_item.php
@@ -0,0 +1,10 @@
+<?php $image = get_field('icon'); ?>
+<div>
+    <h2><?php the_title(); ?></h2>
+    <?php if( $image ): ?>
+      <div>
+        <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+      </div>
+    <?php endif; ?>
+    <?php the_content(); ?>
+</div>
